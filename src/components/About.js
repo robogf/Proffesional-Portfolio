d063282d95c3823ce53bfbb113f7fa8e722a7c27
robogf <111620315+robogf@users.noreply.github.com>
2022-12-05 21:19:11 -0800

    start off about page

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+
+export default function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a full stack web developer with a background in customer service and
+        sales. I have a passion for problem solving and creating solutions. I
+        enjoy learning new technologies and applying them to real world
+        problems.
+      </p>
+    </section>
+  );
+}
