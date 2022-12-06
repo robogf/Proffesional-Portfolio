0b309988acce7420cea7721dc681c4503fecd5e3
robogf <111620315+robogf@users.noreply.github.com>
2022-12-05 22:34:12 -0800

    added a data.js file which allows us to input all the projects we will use

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+        title: "Get Route",
+        subtitle: "Node.js, javascript, handlebars, express, sequelize, mysql",
+        description:"Get route is a fullstack application which allows users to emulate a shopping site. Users can create an account, login, and add items to their cart. Users can also view their order history. The application uses Node.js, javascript, handlebars, express, sequelize, and mysql.",
+        image: "./getroute.png",
+        link: "https://get-route.herokuapp.com/"
+    },
+    {
+        title: "LivHealthy",
+        subtitle: "Html, css, javascript, chart.js, api",
+        description:"LivHealthy is a web application which allows users to search for a food item and view the nutritional information, as well as different types of workouts to enforce a healthy lifestyle. The application uses html, css, javascript, chart.js, and api.",
+        image: "./livhealthy.png",
+        link: "https://angelmatias.github.io/LivHealthy/"
+    },
+    {
+        title: "Employee Tracker",
+        subtitle: "Node.js, javascript, mysql",
+        description:"Employee Tracker is a command line application which allows users to view and manage the departments, roles, and employees in a company. The application uses Node.js, javascript, and mysql.",
+        image: "./employeetracker.png",
+        link: "https://github.com/robogf/Employee-Tracker"
+    },
+    {
+        title: "Password Generator",
+        subtitle: "Html, css, javascript",
+        description:"Password Generator is a web application which allows users to generate a random password based on criteria they’ve selected. The application uses html, css, and javascript.",
+        image: "./passwordgenerator.png",
+        link: "https://angelmatias.github.io/Password-Generator/"
+    }
+]
