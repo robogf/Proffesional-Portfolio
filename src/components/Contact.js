8212ce0ea2b673044edb6327a0254a666bceb8b2
robogf <111620315+robogf@users.noreply.github.com>
2022-12-06 02:56:11 -0800

    fixed submit button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,11 @@ import { validateEmail } from "../utils/helpers";
 function Contact () {
   const [name, setName] = React.useState("");
   
-   const [errorMessage, setErrorMessage] = React.useState('');
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [ message, setMessage] = React.useState('');
 
    const handleSubmit = (e) => {
+        // e.preventDefault();
       if (e.target.name === 'email') {
          const isValid = validateEmail(e.target.value);
          console.log(isValid);
@@ -23,12 +25,18 @@ function Contact () {
       }
       if (!errorMessage) {
          setName({ ...name, [e.target.name]: e.target.value });
-      }
-   }
+      };
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+        setMessage('Form submitted');
+    }
+   
    return (
         <form
         name="contact"
-        onSubmit={handleSubmit}
+        onSubmit={onSubmit}
         className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
         <h2 className="text-white sm:text-4x1 text-3xl mb-1 font-medium title-font">
             Contact Me
@@ -78,10 +86,19 @@ function Contact () {
             </div>          
         )}
         <button
+           
             type="submit"
-            className="text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 rounded text-lg">
+            className="w-half bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+           >
             Submit
+            
         </button>
+      
+        {message && (
+            <div>
+                <p className="success-text">{message}</p>
+            </div>
+        )}
         </form>
    )
 }
